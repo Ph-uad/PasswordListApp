15fc293c7847ca99b36015775af8d81cb97f7dd5
Ph-uad <temidayo26j@gmail.com>
2024-01-03 16:52:01 +0100

    Initial Commit

---
+++ PasswordListApp/PasswordListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordListAppApp.swift
+//  PasswordListApp
+//
+//  Created by ツ水 on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PasswordListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
